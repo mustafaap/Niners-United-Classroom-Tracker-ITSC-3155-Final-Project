44c3a305cd07330933ac81ff2067c63c07f423ab
Henry Chin <hchin3@uncc.edu>
2023-03-28 17:05:49 -0400

    Initial about commit

--- app.py
+++ app.py
@@ -17,4 +17,8 @@ def display_sign_up_page():
 @app.get('/login')
 def login():
     return render_template('login.html')
+
+@app.get('/about')
+def about():
+    return render_template('about.html')
     
