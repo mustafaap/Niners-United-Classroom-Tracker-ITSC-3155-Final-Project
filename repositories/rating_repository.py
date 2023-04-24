e60b67810b2b51418b96c096557648be61aa12d4
AnuragDasari <anuragchowdary@outlook.com>
2023-04-24 16:45:09 -0400

    Delete fixed

--- repositories/rating_repository.py
+++
@@ -1,6 +0,0 @@
-from src.models import db, Rating
-class RatingRepository:
-
-    def delete_rating(self, rating_id):
-        db.session.query(Rating).filter_by(rating_id).delete()
-        db.session.commit()
